6cafd52b03ca1ea03a5197740a26bbb5c4f0e308
K E N <113843247+Ryusei1712@users.noreply.github.com>
2024-05-20 15:47:30 +0700

    First commit

---
+++ src/main/java/com/seafood/management/dacntt/DacnttApplication.java
@@ -0,0 +1,13 @@
+package com.seafood.management.dacntt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DacnttApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DacnttApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/seafood/management/dacntt/DacnttApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seafood.management.dacntt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DacnttApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
